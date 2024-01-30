0fec8f510866d50eb8f3bedf77941ff69912637f
LuminaryChaos <othmanenaji09@gmail.com>
2024-01-30 10:15:02 +0100

    first test

---
+++ Seance01/seance01.js
@@ -0,0 +1,27 @@
+function othmane(a){
+    console.log("Mahdakx lah ya " + a)
+}
+othmane("aYman")
+
+
+
+
+
+function somme(p,q){
+    return p + q
+}
+console.log (somme(2,12))
+
+
+
+const PI = 3.14
+
+functioninscrire(){
+    let nom;
+    let prenom;
+    let age;
+    "Najmi";
+    "Othmane";
+    age=21;
+}
+
---
+++ Seance02/seance02.js
@@ -0,0 +1,78 @@
+// example number 1
+
+
+let x = 1+1
+let y = "5" + 1
+
+let texte1 ="OFPPT" ;  
+texte1 += " "
+let texte2 = "en force";
+let  texte3 = texte1 + texte2;
+
+console.log(texte3) 
+
+
+
+// example number 2  ( "==" : même valeur and "===" : même valeur et même type) 
+
+let a = 5;
+let b = "5";
+
+if (a === b){
+    console.log("YES");
+}
+else{
+    console.log("NO");
+};
+
+
+
+
+
+
+// example number 3 ("!=" : même valeur and "!==" : d valeur et même type) 
+
+let r = 5;
+let e = "5";
+
+if (e != r){
+    console.log("YES");
+}
+else{
+    console.log("NO");
+};
+
+
+
+// example number 4  
+
+let c = 5;
+let d = "5";
+
+if (c !== d){
+    console.log("YES");
+}
+else{
+    console.log("NO");
+};
+
+
+
+
+
+
+
+
+
+// example of
+
+let statut = "etudiant";
+let age ="18";
+
+if ( age == 20 || statut == "etudiant"){
+    console.log("tu es obligé de faire ce test 1")
+}else{
+    console.log("prepare toi a ce test la semaine prochaine 1")
+};
+
+
