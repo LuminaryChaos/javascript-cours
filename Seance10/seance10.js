e304723940169063e2d5968c7db173cf1e6f2c96
LuminaryChaos <othmanenaji09@gmail.com>
2024-02-22 14:42:36 +0100

    seance 11

--- Seance10/seance10.js
+++ Seance10/seance10.js
@@ -14,43 +14,62 @@
 
             stagiaireslist.push(stagiaire);
         })
+
         document.querySelector(".afficher").addEventListener("click", (e) => {
             e.preventDefault();
 
 
-        let table = document.createElement('table');
-            table.style.border = '1px solid black';
+                let table = document.createElement('table');
+                    table.style.border = '1px solid black';
 
-        let headerRow = table.insertRow();
-        let headers = ['Nom', 'Prenom', 'Age'];
+                let headerRow = table.insertRow();
+                let headers = ['Nom', 'Prenom', 'Age'];
 
-            headers.forEach(headerText => {
-                let header = document.createElement('th');
-                let textNode = document.createTextNode(headerText);
-                header.appendChild(textNode);
-                headerRow.appendChild(header);
+                    headers.forEach(headerText => {
+                        let header = document.createElement('th');
+                        let textNode = document.createTextNode(headerText);
+                        header.appendChild(textNode);
+                        headerRow.appendChild(header);
+                    });
+
+
+                    stagiaireslist.forEach(stagiaire => {
+                        let row = table.insertRow();
+                        let nomCell = row.insertCell();
+                        let prenomCell = row.insertCell();
+                        let ageCell = row.insertCell();
+
+                        nomCell.appendChild(document.createTextNode(stagiaire.nom));
+                        prenomCell.appendChild(document.createTextNode(stagiaire.prenom));
+                        ageCell.appendChild(document.createTextNode(stagiaire.age));
+
+
+                        nomCell.style.border = '1px solid black';
+                        prenomCell.style.border = '1px solid black';
+                        ageCell.style.border = '1px solid black';
+                    });
+
+
+                    let displayDiv = document.createElement("div");
+                    displayDiv.classList.add("table-container");
+                    displayDiv.appendChild(table);
+                    document.body.appendChild(displayDiv);
+
+            });
+
+            document.querySelector(".dlt-table").addEventListener("click", (e) => {
+                e.preventDefault();
+        
+                const tableContainers = document.querySelectorAll(".table-container");
+                tableContainers.forEach(elem => {
+                    elem.remove();
+                });
             });
 
 
-            stagiaireslist.forEach(stagiaire => {
-                let row = table.insertRow();
-                let nomCell = row.insertCell();
-                let prenomCell = row.insertCell();
-                let ageCell = row.insertCell();
+            let days = [1,2,3,4,5,6,7]
 
-                nomCell.appendChild(document.createTextNode(stagiaire.nom));
-                prenomCell.appendChild(document.createTextNode(stagiaire.prenom));
-                ageCell.appendChild(document.createTextNode(stagiaire.age));
-
-
-                nomCell.style.border = '1px solid black';
-                prenomCell.style.border = '1px solid black';
-                ageCell.style.border = '1px solid black';
-            });
-
-
-            let displayDiv = document.createElement("div");
-            displayDiv.classList.add("table-container");
-            displayDiv.appendChild(table);
-            document.body.appendChild(displayDiv);
-        });
+            days.forEach(elem => {
+                console.log(elem)
+            })
+            
---
+++ Seance11/seance11.js
@@ -0,0 +1,46 @@
+
+const parent  = document.querySelector(".parent");
+const input = document.querySelector('input[type="Text"]')
+
+// mouseenter & mouseleave = parent
+// mouseover  & mouseout  =parent + children
+
+// parent.addEventListener("mouseenter", () =>{ // will affich only when you enter to the parent not the children
+//     console.log("hello")
+// })
+
+// parent.addEventListener("mouseover", () =>{ // will affich every time you put the mouse on the childern or the parent
+//     console.log("hello")
+// })
+
+// parent.addEventListener("mouseleave", () =>{ // will affich when you go out of the parent 
+//      console.log("hello")
+// })
+
+// parent.addEventListener("mouseout", () =>{ // will affich when you go out of the parent even if you go to the childern 
+//     console.log("hello")
+// })
+
+// parent.addEventListener("mousedown", () =>{  // fach katwark 3la souri lakin matla9tich lidk
+//     console.log("daba nta mwark")
+// })
+
+// parent.addEventListener("mouseup", () =>{   // fach kat7ayd lidk min dagta
+//     console.log("daba 7aydti min tawrika")
+// })
+
+// parent.addEventListener("dblclick", () =>{   // affiche when you click tow times
+//     console.log("you clicked two times")
+// })
+
+// parent.addEventListener("mousemove", () =>{   // m7adk nta kat7ark o hia khdama kataficcher
+//     console.log("ra kant7ark daba")
+// })
+
+// input.addEventListener('focus',()=>{    // aficher when you are focusing
+//     console.log("ma7ba bik f input")
+// })
+
+// input.addEventListener('blur',()=>{     // aficher when you stop focusing
+//     console.log("safi lach dakhli mli bagi tkhroj hhhhh")
+// })
